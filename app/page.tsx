07f34206ff2bc808932722ca5a8747f79a29b3dc
Tharinda Anurajeewa <tharindahp@gmail.com>
2025-02-17 23:14:49 +0530

    Add About and Contact Us sections to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import { AboutSection } from '@/components/about-section';
+import { ContactUsSection } from '@/components/contact-us-section';
 import { EventsSection } from '@/components/events-section';
 import { MembersSection } from '@/components/memebrs-section';
 
 export default function Home() {
   return (
     <div className=''>
+      <AboutSection />
       <EventsSection />
       <MembersSection />
+      <ContactUsSection />
     </div>
   );
 }
