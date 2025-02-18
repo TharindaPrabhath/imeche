2c6b5ba9d0507b44e0cc95195a6e61961288ec41
Tharinda Anurajeewa <tharindahp@gmail.com>
2025-02-18 19:08:22 +0530

    Add comprehensive SEO and Open Graph metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,38 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'IMechE Student Chapter of UoM',
   description: 'The Institution of Mechanical Engineers Student Chapter of the University of Moratuwa',
-  keywords: ['IMechE', 'UoM', 'University of Moratuwa', 'Mechanical Engineering', 'Student Chapter']
+  keywords: ['IMechE', 'UoM', 'University of Moratuwa', 'Mechanical Engineering', 'Student Chapter'],
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://imeche.uom.lk',
+    siteName: 'IMechE Student Chapter of UoM',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/drifly/image/upload/v1739885103/IMechE_atjivb.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'IMechE Student Chapter of UoM'
+      }
+    ]
+  },
+  twitter: {
+    site: '@imeche_uom',
+    images: ['https://res.cloudinary.com/drifly/image/upload/v1739885103/IMechE_atjivb.jpg']
+  },
+  authors: [
+    {
+      name: 'Tharinda Anurajeewa',
+      url: 'https://www.linkedin.com/in/tharinda-anurajeewa'
+    }
+  ],
+  creator: 'Tharinda Anurajeewa',
+  publisher: 'IMechE Student Chapter of UoM',
+  category: 'Engineering',
+  abstract: 'The Institution of Mechanical Engineers Student Chapter of the University of Moratuwa',
+  metadataBase: new URL('https://imeche.uom.lk'),
+  bookmarks: ['https://imeche.uom.lk'],
+  manifest: '/site.webmanifest'
 };
 
 export default function RootLayout({
@@ -26,7 +57,7 @@ export default function RootLayout({
       <body className={`${inter.className} flex min-h-screen flex-col antialiased`}>
         <Header />
         <main className='h-full'>{children}</main>
-        <Footer className='' />
+        <Footer />
       </body>
     </html>
   );
