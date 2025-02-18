b02b3cfb581aa9a5b77f17930addd939f220df85
Tharinda Anurajeewa <tharindahp@gmail.com>
2025-02-18 18:18:07 +0530

    Add Hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { AboutSection } from '@/components/about-section';
 import { ContactUsSection } from '@/components/contact-us-section';
 import { EventsSection } from '@/components/events-section';
+import { HeroSection } from '@/components/Hero-section';
 import { MembersSection } from '@/components/memebrs-section';
 
 export default function Home() {
   return (
-    <div className=''>
+    <>
+      <HeroSection />
       <AboutSection />
       <EventsSection />
       <MembersSection />
       <ContactUsSection />
-    </div>
+    </>
   );
 }
