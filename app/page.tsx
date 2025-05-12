4711de9657e6af53bf3e2569a9765f594e66cb4b
Tharinda Anurajeewa <tharindahp@gmail.com>
2025-05-12 16:01:44 +0530

    Update members

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { AboutSection } from '@/components/about-section';
 import { ContactUsSection } from '@/components/contact-us-section';
 import { EventsSection } from '@/components/events-section';
 import { HeroSection } from '@/components/Hero-section';
-import { MembersSection } from '@/components/memebrs-section';
+import { MembersSection } from '@/components/members-section';
 
 export default function Home() {
   return (
